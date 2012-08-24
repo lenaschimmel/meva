8aa0f17db93434a392d6b042e724b628845f93d4
Lena Schimmel <schimmel@gmino.de>
2012-08-24 18:47:06 +0200

    Added caching for loadEntities and getIdsByQuery on android.

--- meva.module/android/src/de/gmino/meva/android/request/NetworkRequestsImplAsyncTaskBinaryHttp.java
+++ meva.module/android/src/de/gmino/meva/android/request/NetworkRequestsImplAsyncTaskBinaryHttp.java
@@ -3,10 +3,13 @@ package de.gmino.meva.android.request;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -34,6 +37,8 @@ import de.gmino.meva.shared.request.RequestListener;
 public class NetworkRequestsImplAsyncTaskBinaryHttp implements NetworkRequests {
 
 	String baseUrl;
+	
+	Map<Integer, Collection<Long>> queryCache = new TreeMap<Integer, Collection<Long>>();
 
 	public NetworkRequestsImplAsyncTaskBinaryHttp(String baseUrl) {
 		this.baseUrl = baseUrl;
@@ -41,51 +46,70 @@ public class NetworkRequestsImplAsyncTaskBinaryHttp implements NetworkRequests {
 
 	@Override
 	public void getIdsByQuery(Query query, final RequestListener<Long> listener) {
-		new AsyncTask<Query, Long, Collection<Long>>() {
-
-			protected Collection<Long> doInBackground(Query... queryParams) {
-				Query query = queryParams[0];
-				Collection<Long> ids = new LinkedList<Long>();
-
-				try {
-					HttpClient client = new DefaultHttpClient();
-					HttpPost request = new HttpPost();
-
-					// TODO why is the actual connection within this Request
-					// class, even though other request classes delegate that to
-					// Impl classes?
-
-					request.setURI(new URI(Util.getBaseUrl() + "Binary/"
-							+ query.getUrlPostfix()));
-
-					ByteArrayOutputStream baos = new ByteArrayOutputStream();
-					DataOutputStream dos = new DataOutputStream(baos);
-					((ValueBinary) query).serializeBinary(dos);
-
-					HttpEntity postBody = new ByteArrayEntity(
-							baos.toByteArray());
-					request.setEntity(postBody);
-
-					HttpResponse response = client.execute(request);
-					DataInputStream dis = new DataInputStream(response
-							.getEntity().getContent());
-
-					long id = dis.readLong();
-					while (id != 0) {
-						ids.add(id);
-						publishProgress(id);
-						id = dis.readLong();
-					}
-				} catch (Exception e) {
-					throw new RuntimeException(e);
-				}
-				return ids;
-			};
-
-			protected void onPostExecute(java.util.Collection<Long> result) {
+		StringBuilder sb = new StringBuilder();
+		try {
+			query.serializeJson(sb);
+			final int queryHash = sb.toString().hashCode();
+			Collection<Long> result = queryCache.get(queryHash);
+			if(result != null)
+			{
 				listener.onFinished(result);
-			};
-		}.execute(query);
+				return;
+			}
+			else
+			{
+				new AsyncTask<Query, Long, Collection<Long>>() {
+
+					protected Collection<Long> doInBackground(Query... queryParams) {
+						Query query = queryParams[0];
+						Collection<Long> ids = new LinkedList<Long>();
+
+						try {
+							HttpClient client = new DefaultHttpClient();
+							HttpPost request = new HttpPost();
+
+							// TODO why is the actual connection within this Request
+							// class, even though other request classes delegate that to
+							// Impl classes?
+
+							request.setURI(new URI(Util.getBaseUrl() + "Binary/"
+									+ query.getUrlPostfix()));
+
+							ByteArrayOutputStream baos = new ByteArrayOutputStream();
+							DataOutputStream dos = new DataOutputStream(baos);
+							((ValueBinary) query).serializeBinary(dos);
+
+							HttpEntity postBody = new ByteArrayEntity(
+									baos.toByteArray());
+							request.setEntity(postBody);
+
+							HttpResponse response = client.execute(request);
+							DataInputStream dis = new DataInputStream(response
+									.getEntity().getContent());
+
+							long id = dis.readLong();
+							while (id != 0) {
+								ids.add(id);
+								publishProgress(id);
+								id = dis.readLong();
+							}
+						} catch (Exception e) {
+							throw new RuntimeException(e);
+						}
+						queryCache.put(queryHash, ids);
+						return ids;
+					};
+
+					protected void onPostExecute(java.util.Collection<Long> result) {
+						listener.onFinished(result);
+					};
+				}.execute(query);
+			}
+		} catch (IOException e1) {
+			throw new RuntimeException(e1);
+		}
+		
+	
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
@@ -134,7 +158,25 @@ public class NetworkRequestsImplAsyncTaskBinaryHttp implements NetworkRequests {
 	public <EntityClass extends Entity> void loadEntities(
 			final Collection<EntityClass> entities,
 			final RequestListener<EntityClass> listener) {
+		
+		Collection<EntityClass> unloadedEntities = null;
+		
+		for(EntityClass e : entities)
+		{
+			if(!e.isReady())
+			{
+				if(unloadedEntities == null)
+					unloadedEntities = new LinkedList<EntityClass>();
+				unloadedEntities.add(e);
+			}
+		}
 
+		if(unloadedEntities == null || unloadedEntities.isEmpty())
+		{
+			listener.onFinished(entities);
+			return;
+		}
+		
 		new AsyncTask<Collection<EntityClass>, EntityClass, Collection<EntityClass>>() {
 			protected Collection<EntityClass> doInBackground(
 					Collection<EntityClass>... entitiesParams) {
@@ -175,7 +217,7 @@ public class NetworkRequestsImplAsyncTaskBinaryHttp implements NetworkRequests {
 			protected void onPostExecute(java.util.Collection<EntityClass> result) {
 				listener.onFinished(result);
 			};
-		}.execute(entities);
+		}.execute(unloadedEntities);
 	}
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
