f810c92280e14335bceac2da727983e53a954099
Lena Schimmel <schimmel@gmino.de>
2013-09-02 21:06:17 +0200

    Added factory impl for ios.

---
+++ issuemap.module/ios/src/de/gmino/issuemap/ios/EntityFactoryImpl.java
@@ -0,0 +1,123 @@
+package de.gmino.issuemap.ios;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.itemscript.core.values.JsonObject;
+
+import de.gmino.geobase.ios.domain.Address;
+import de.gmino.geobase.ios.domain.Distance;
+import de.gmino.geobase.ios.domain.Duration;
+import de.gmino.geobase.ios.domain.ImageUrl;
+import de.gmino.geobase.ios.domain.LatLon;
+import de.gmino.geobase.ios.domain.Timestamp;
+import de.gmino.issuemap.ios.domain.Comment;
+import de.gmino.issuemap.ios.domain.DecentralizedGeneration;
+import de.gmino.issuemap.ios.domain.Map;
+import de.gmino.issuemap.ios.domain.Markertype;
+import de.gmino.issuemap.ios.domain.Photo;
+import de.gmino.issuemap.ios.domain.Poi;
+import de.gmino.issuemap.ios.domain.Route;
+import de.gmino.issuemap.ios.request.QueryMapBySubdomain;
+import de.gmino.issuemap.ios.request.SendFeedback;
+import de.gmino.meva.ios.domain.Date;
+import de.gmino.meva.ios.domain.DateTime;
+import de.gmino.meva.ios.domain.KeyValueDef;
+import de.gmino.meva.ios.domain.KeyValueSet;
+import de.gmino.meva.ios.domain.LongText;
+import de.gmino.meva.ios.domain.ShortText;
+import de.gmino.meva.ios.domain.Time;
+import de.gmino.meva.shared.Entity;
+import de.gmino.meva.shared.EntityFactoryInterface;
+import de.gmino.meva.shared.TypeName;
+import de.gmino.meva.shared.Value;
+
+public class EntityFactoryImpl implements EntityFactoryInterface {
+
+	public EntityFactoryImpl() {
+		try {
+			createValueObjectFromJson(null, null);
+		} catch (Exception e) {
+			// Exception always occurs, but this is no error
+		}
+	}
+	
+	@Override
+	public Collection<Entity> createEntityObjects(String typeName, Collection<Long> ids) {
+		Collection<Entity> ret = new ArrayList<Entity>(ids.size());
+		for (long id : ids) {
+			ret.add(createEntityObject(typeName, id));
+		}
+		return ret;
+	}
+
+	// TODO autocreate this class, at least the method below
+
+	@Override
+	public Entity createEntityObject(String typeName, long id) {
+		if (typeName.equals("Poi"))
+			return new Poi(id);
+		if (typeName.equals("Map"))
+			return new Map(id);
+		if (typeName.equals("Markertype"))
+			return new Markertype(id);
+		if (typeName.equals("Comment"))
+			return new Comment(id);
+		if (typeName.equals("Photo"))
+			return new Photo(id);
+		if (typeName.equals("Route"))
+			return new Route(id);
+		if (typeName.equals("KeyValueSet"))
+			return new KeyValueSet(id);
+		if (typeName.equals("KeyValueDef"))
+			return new KeyValueDef(id);
+		if (typeName.equals("DecentralizedGeneration"))
+			return new DecentralizedGeneration(id);
+		throw new RuntimeException("Unsupported Entity type: " + typeName);
+	}
+	
+	@Override
+	public Object createQueryObject(String typeName, Object request) throws IOException {
+		if(!(request instanceof JsonObject))
+			throw new RuntimeException("On the client side, requestObject must be of type JsonObject.");
+		
+		if (typeName.equals("QueryMapBySubdomain"))
+			return new QueryMapBySubdomain((JsonObject)request);
+		else if (typeName.equals("SendFeedback"))
+			return new SendFeedback((JsonObject)request);
+		else
+			throw new RuntimeException("Unrecognized query type: " + typeName);
+	}
+
+	@Override
+	public Value createValueObjectFromJson(TypeName type, JsonObject json) {
+		try {
+			if(type == LatLon.type)
+				return new LatLon(json);
+			if(type == Timestamp.type)
+				return new Timestamp(json);
+			if(type == Address.type)
+				return new Address(json);
+			if(type == Distance.type)
+				return new Distance(json);
+			if(type == Distance.type)
+				return new Duration(json);
+			if(type == ImageUrl.type)
+				return new ImageUrl(json);
+			if(type == LongText.type)
+				return new LongText(json);
+			if(type == ShortText.type)
+				return new ShortText(json);
+			if(type == Date.type)
+				return new Date(json);
+			if(type == Time.type)
+				return new Time(json);
+			if(type == DateTime.type)
+				return new DateTime(json);
+		} catch (IOException e) {
+			throw new RuntimeException("Error while deserializing: " + json);
+		}
+		throw new RuntimeException("Unsupported Value type: " + type);
+	}
+}
